fbfc6b180d3ffb663bbc81f721961502998781dd
hallefcalves <hallef28@gmail.com>
2022-09-01 22:19:31 -0300

    url

--- http/web.dart
+++ http/web.dart
@@ -1,51 +1,54 @@
 import 'dart:convert';
-
 import 'package:http/http.dart' as http;
 
-Future<String?> fetchData(codigo) async {
-  var request = http.Request(
-      'GET',
-      Uri.parse(
-          'http://localhost:1026/v2/entities/urn:ngsi-ld:Shelf:unit001/?options=values&attrs=refStore'));
-  request.body = '''''';
+class orion {
+  var url = "20.163.17.242";
 
-  http.StreamedResponse response = await request.send();
+  Future<String?> fetchData(codigo) async {
+    var request = http.Request(
+        'GET',
+        Uri.parse(
+            'http://$url:1026/v2/entities/urn:ngsi-ld:Shelf:unit001/?options=values&attrs=refStore'));
+    request.body = '''''';
 
-  if (response.statusCode == 200) {
-    print(await response.stream.bytesToString());
-  } else {
-    print(response.reasonPhrase);
+    http.StreamedResponse response = await request.send();
+
+    if (response.statusCode == 200) {
+      print(await response.stream.bytesToString());
+    } else {
+      print(response.reasonPhrase);
+    }
+  }
+
+  sendData() async {
+    var headers = {'Content-Type': 'application/json'};
+    var request = http.Request('POST', Uri.parse('http://$url:1026/v2/op/update'));
+    request.body = json.encode({
+      "id": "urn:ngsi-ld:remedio:001",
+      "type": "remedio",
+      "name": {"type": "string", "value": "Advil"},
+      "imagem": {
+        "type": "string",
+        "value":
+            "https://www.erifarma.com.br/medicamentos/advil-400mg-20-capsulas"
+      },
+      "lote": {"type": "Text", "value": "AR750"},
+      "qtdPilulas": {"type": "Integer", "value": 8},
+      "dataValidade": {"type": "date", "value": "22/07/2029"},
+      "refIdoso": {"type": "Relationship", "value": "urn:ngsi-ld:Idoso:001"}
+    });
+    request.headers.addAll(headers);
+
+    http.StreamedResponse response = await request.send();
+
+    if (response.statusCode == 200) {
+      print(await response.stream.bytesToString());
+    } else {
+      print(response.reasonPhrase);
+    }
   }
 }
 
-sendData() async {
-  var headers = {'Content-Type': 'application/json'};
-  var request =
-      http.Request('POST', Uri.parse('http://localhost:1026/v2/op/update'));
-  request.body = json.encode({
-    "id": "urn:ngsi-ld:remedio:001",
-    "type": "remedio",
-    "name": {"type": "string", "value": "Advil"},
-    "imagem": {
-      "type": "string",
-      "value":
-          "https://www.erifarma.com.br/medicamentos/advil-400mg-20-capsulas"
-    },
-    "lote": {"type": "Text", "value": "AR750"},
-    "qtdPilulas": {"type": "Integer", "value": 8},
-    "dataValidade": {"type": "date", "value": "22/07/2029"},
-    "refIdoso": {"type": "Relationship", "value": "urn:ngsi-ld:Idoso:001"}
-  });
-  request.headers.addAll(headers);
-
-  http.StreamedResponse response = await request.send();
-
-  if (response.statusCode == 200) {
-    print(await response.stream.bytesToString());
-  } else {
-    print(response.reasonPhrase);
-  }
-}
 
 /* todos> dataCriacao:data
 ?todos> ultimaAtualizacao:data
