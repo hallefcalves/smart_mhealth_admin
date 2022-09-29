ff8c12dbbe73a71ca53c468419f0f58803452526
kaiantiago <kaiantiago@outlook.com>
2022-09-28 22:20:57 -0300

    adjustment on the Elevated Button from Colaborators

--- lib/screens/colaboradores.dart
+++ lib/screens/colaboradores.dart
@@ -61,22 +61,26 @@ class Colaboradores extends StatelessWidget {
           ),
           CardColaborador(""),
           CardColaborador(""),
-          ElevatedButton(
-            onPressed: () => {
-              Navigator.push(
-                  context,
-                  MaterialPageRoute(
-                      builder: (context) => const CadastroCuidador()))
-            },
-            style: ElevatedButton.styleFrom(
-              primary: MyTheme.defaultTheme.primaryColor,
-              minimumSize: const Size(80, 40),
-              maximumSize: const Size(80, 40),
-              shape: RoundedRectangleBorder(
-                borderRadius: BorderRadius.circular(30),
+          Padding(
+            padding: EdgeInsetsDirectional.fromSTEB(70, 30, 70, 0),
+            child: ElevatedButton(
+              onPressed: () => {
+                Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                        builder: (context) => const CadastroCuidador()))
+              },
+              style: ElevatedButton.styleFrom(
+                primary: MyTheme.defaultTheme.primaryColor,
+                minimumSize: const Size(80, 40),
+                maximumSize: const Size(80, 40),
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(30),
+                ),
               ),
+              child: const Text('+ Cadastrar Colaborador',
+                  style: TextStyle(fontSize: 17)),
             ),
-            child: const Text('Cadastrar Colaborador'),
           ),
         ],
       ),
