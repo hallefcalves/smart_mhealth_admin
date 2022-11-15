e0f2da54307eecd4834074a8095b8c478c618a97
hallefalves <hallef28@gmail.com>
2022-11-15 00:25:35 -0300

    icon and app name

--- lib/components/box_remedio.dart
+++ lib/components/box_remedio.dart
@@ -1,11 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:smart_mhealth_admin/http/alarme/alarme.dart';
 import 'package:smart_mhealth_admin/http/ingestao_remedio/ingestao_remedio.dart';
-import 'package:smart_mhealth_admin/http/remedio/remedio.dart';
-import 'package:smart_mhealth_admin/http/remedio/web_remedio.dart';
 import 'package:smart_mhealth_admin/themes/color.dart';
-
-import '../http/agenda/agenda.dart';
 import '../http/ingestao_remedio/web_ingestao_remedio.dart';
 
 class BoxRemedio extends StatelessWidget {
